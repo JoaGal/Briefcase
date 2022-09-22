2506ff2c05e216e747d98178df37b7c1f8819e5d
JoaGal <joaquingaldeano7@gmail.com>
2022-09-22 11:39:35 -0300

    Commit

--- src/Components/ProyectsItems/ProyectsItems.js
+++ src/Components/ProyectsItems/ProyectsItems.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './ProyectsItems.css'
+import './ProyectsItems.css';
+import { AiOutlineLink } from 'react-icons/ai';
 
 export default function ProjectsItems({
 
@@ -10,9 +11,10 @@ export default function ProjectsItems({
   return (
       <div className='projectItem__img'>
         <img src={img} alt="project img" className='project__img' />
-        <div className="projectItem__info">
+        <div className='projectItem__info'>
           <h3 className="projectItem__title">{title}</h3>
-          <p className="projectItem__desc">{desc}</p>
+          <p className='projectItem__desc'>{desc}</p>
+          <div className='projectItem__desc__link'><AiOutlineLink className='projectItem__ico'/></div>
         </div>
       </div>
 
